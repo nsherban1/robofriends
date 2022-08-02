aad0cfbae28efcf549283ec84ef54e6668787172
nsherban1 <56939403+nsherban1@users.noreply.github.com>
2022-08-02 00:54:15 -0600

    Configure robofriends for deployment

---
+++ src/robots.js
@@ -0,0 +1,75 @@
+export const robots = [
+{
+	id:0,
+	name:'Roderick Buntley',
+	username: 'Rod',
+	email: 'Rbuntley@email.biz'
+},
+{
+	id:1,
+	name:'Lim Jahey',
+	username: 'Jim',
+	email: 'jimmyl@email.biz'
+},
+{
+	id:2,
+	name:'Marta Souberne',
+	username: 'MSoub',
+	email: 'Msoub@email.biz'
+},
+{
+	id:3,
+	name:'Elisa Widget',
+	username: 'Elwidg',
+	email: 'Elwidg@email.biz'
+},
+{
+	id:4,
+	name:'Randy Bobandy',
+	username: 'Rand',
+	email: 'bobandus@email.biz'
+},
+{
+	id:5,
+	name:'Julian Elderman',
+	username: 'Julz',
+	email: 'jelderm@email.biz'
+},
+{
+	id:6,
+	name:'Fiorentina Di Matteo',
+	username: 'FioDM',
+	email: 'fdimatteo@email.biz'
+},
+{
+	id:7,
+	name:'Fritz Fritzpatrick',
+	username: 'Fritzy',
+	email: 'fritzpatrick@email.biz'
+},
+{
+	id:8,
+	name:'Ahmed Salah',
+	username: 'ASalah',
+	email: 'asalah@email.biz'
+},
+{
+	id:9,
+	name:'Patricia Rumple',
+	username: 'Prumple',
+	email: 'prumple@email.biz'
+},
+{
+	id:10,
+	name:'Carlos Rodriguez',
+	username: 'CRodriguez',
+	email: 'crodriguez12@email.biz'
+},
+
+
+
+
+
+
+
+]
